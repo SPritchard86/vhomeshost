7f309678096863506359cfc68793d7db46a297e1
Steve <stephen.pritchard@my.jcu.edu.au>
2019-07-01 13:52:40 +1000

    Create base class for customer.

--- customer.py
+++ customer.py
@@ -1,10 +1,22 @@
 class Customer():
 
-    def __init__(self, interest_type, entry_time):
+    def __init__(self, fname = "", lname = "", address = "", mobile_phone = "", work_phone = "", home_phone = "", home_fax = "", work_fax = "", email = "", house_land_budget = 0, house_only_budget = 0, is_selling_existing = FALSE, land_details = "", notes = ""):
         '''constructor'''
-        self.interest_type = interest_type
-        self.entry_time = entry_time
+        self.fname = fname
+        self.lname = lname
+        self.address = address
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.home_phone = home_phone
+        self.home_fax = home_fax
+        self.work_fax = work_fax
+        self.email = email
+        self.house_land_budget = house_land_budget
+        self.house_only_budget = house_only_budget
+        self.is_selling_existing = is_selling_existing
+        self.land_details = land_details
+        self.notes = notes
+
 
     def __str__(self):
-        return ""
-
+        return "Name: {} {} Address: {} Ph(M): {}, Ph(W): {} Ph(H): {} Fax(H): {} Fax(W): {} Email: {} House/Land budget: {} House only budget: {} Selling existing: {} Land details: {} Notes: {}".format(self.fname, self.lname, self.address, self.mobile_phone, self.work_phone, self.home_phone, self.home_fax, self.work_fax, self.email, self.house_land_budget, self.house_only_budget, self.is_selling_existing, self.land_details, self.notes)
--- customer_counter.py
+++ customer_counter.py
@@ -4,7 +4,7 @@ Author: Stephen Pritchard
 Date: 1/07/2019
 """
 
-
+from customer import Customer
 import datetime
 
 REPORT_DAYS = ["Sunday", "Tuesday"]
@@ -33,7 +33,20 @@ def main():
             print("Potential build customer view times:")
             print(build_view_times)
         elif menu_choice == 4:
-            pass
+            print_customer_menu()
+            cust_menu_choice = int(input(">>> "))
+            while cust_menu_choice != 4:
+                if cust_menu_choice == 1:
+                    create_customer()
+                elif cust_menu_choice == 2:
+                    pass
+                elif cust_menu_choice == 3:
+                    pass
+                else:
+                    print("Invalid option")
+                print_customer_menu()
+                cust_menu_choice = int(input(">>> "))
+
         elif menu_choice == 5:
             pass
         else:
@@ -51,7 +64,7 @@ def print_menu(casual_count, build_count):
         print("Today is a reporting day. Make sure to report after 3:30pm!")
     print("Current casual customer count: {}".format(casual_count))
     print("Current potential 6-12 months build customer count: {}".format(build_count))
-    print("_________MENU__________")
+    print("________________MENU________________")
     print("1. Count casual customer")
     print("2. Count potential build customer")
     print("3. View entry times")
@@ -59,9 +72,39 @@ def print_menu(casual_count, build_count):
     print("5. Send weekly report")
     print("6. save and exit")
 
+
+def print_customer_menu():
+    print("__________CUSTOMER DETAILS__________")
+    print("1. Add new customer")
+    print("2. Edit customer details")
+    print("3. Remove customer")
+    print("4. Return to main menu")
+
 def get_current_time():
     current_datetime = datetime.datetime.now()
     return current_datetime.strftime("%X")
+
+def create_customer():
+    fname = input("First name: ")
+    lname = input("Last name: ")
+    address = input("Address: ")
+    mobile_phone = input("Phone number(M): ")
+    work_phone = input("Phone number(W): ")
+    home_phone = input("Phone number(H): ")
+    home_fax = input("Fax number(H): ")
+    work_fax = input("Fax number(W): ")
+    email = input("Email address: ")
+    house_land_budget = input("Budget (House + Land): ")
+    house_only_budget = input("Budget (House only): ")
+    is_selling_existing = input("Must sell existing house(Y/N): ")
+    if is_selling_existing.lower() == "y":
+        is_selling_existing = True
+    else:
+        is_selling_existing = False
+    land_details = input("Land details: ")
+    notes = input("notes: ")
+    return Customer(fname, lname, address, mobile_phone, work_phone, home_phone, home_fax, work_fax, email, house_land_budget, house_only_budget, is_selling_existing, land_details, notes)
+
 
 
 main()
