696c1c2221942d012c60f95df1d7b7f13939b837
Steve <stephen.pritchard@my.jcu.edu.au>
2019-07-01 11:49:21 +1000

    create intitial customer_counter.py file. Add datetime.

---
+++ customer.py
@@ -0,0 +1,10 @@
+class Customer():
+
+    def __init__(self, interest_type, entry_time):
+        '''constructor'''
+        self.interest_type = interest_type
+        self.entry_time = entry_time
+
+    def __str__(self):
+        return ""
+
