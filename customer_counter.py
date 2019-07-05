f701ef53bc854fb0e2d7eb2a15466459eb79d948
Steve <stephen.pritchard@my.jcu.edu.au>
2019-07-05 15:28:50 +1000

    Add ability to collate data from file to get last 7 days of information and present nicely.

--- customer_counter.py
+++ customer_counter.py
@@ -20,6 +20,7 @@ PASSWORD = 'Value2019'
 HOST_NAME = "Steve Pritchard"
 LOCATION = "Elliot Springs"
 
+
 CUSTOMER_TEMPLATE = """Dear {CONSULTANT},
 
 A customer would like for you to contact them in relation to answering enquiries about a project in {LOCATION}.
@@ -46,6 +47,25 @@ Selling existing property: {SELLING_EXISTING}
 Yours Truly,
 {HOST_NAME}"""
 
+REPORT_TEMPLATE_1 = """Dear {CONSULTANT},
+
+Following is a summary of the last 7 days of customer activity at the open home in {LOCATION}.
+
+"""
+
+REPORT_TEMPLATE_2 = """{DAY} {DATE}:
+Customers just looking: {CASUAL_CUSTOMERS}
+Customers potentially building: {BUILD_CUSTOMERS}
+Entry times: {TIMES}
+__________________________________________________
+"""
+
+
+REPORT_TEMPLATE_3 = """
+
+Yours Truly,
+{HOST_NAME}"""
+
 
 
 def main():
@@ -58,12 +78,15 @@ def main():
     build_customer_count = 0
     view_times = []
     customers = []
+    current_datetime = datetime.datetime.now()
+    today = current_datetime.strftime("%A")
+    todays_date = current_datetime.strftime("%x")
 
     print("--- Host Companion App ---")
-    print_menu(casual_customer_count, build_customer_count)
+    print_menu(casual_customer_count, build_customer_count, today)
     menu_choice = int(input(">>> "))
 
-    while menu_choice != 7:
+    while menu_choice != 8:
         if menu_choice == 1:
             casual_customer_count += 1
             view_times.append(get_current_time())
@@ -78,7 +101,7 @@ def main():
             list_customers(customers)
             print_customer_menu()
             cust_menu_choice = int(input(">>> "))
-            while cust_menu_choice != 6:
+            while cust_menu_choice != 7:
                 list_customers(customers)
                 if cust_menu_choice == 1:
                     if len(customers) >= 1:
@@ -145,10 +168,6 @@ def main():
                     customer = int(input("Enter customer number to forward via email: "))
                     further_info = input("Please enter any additional customer information: ")
                     msg = MIMEMultipart()  # create a message
-                    # message = message_template.substitute(CONSULTANT=CONSULTANT_NAME, LOCATION=LOCATION, FIRST_NAME=customers[customer - 1].fname, LAST_NAME=customers[customer - 1].lname, EMAIL=customers[customer - 1].email, ADDRESS=customers[customer - 1].address, WORK_NUMBER=customers[customer - 1].work_phone, HOME_NUMBER=customers[customer - 1].home_phone, MOBILE_NUMBER=customers[customer - 1].mobile_phone, WORK_FAX=customers[customer - 1].work_fax, HOME_FAX=customers[customer - 1].home_fax, HOUSE_LAND_BUDGET=customers[customer - 1].house_land_budget, HOUSE_ONLY_BUDGET=customers[customer - 1].house_only_budget, SELLING_EXISTING=customers[customer - 1].is_selling_existing, LAND_DETAILS=customers[customer - 1].land_details, NOTES=customers[customer - 1].notes, FURTHER_INFO=further_info, HOST_NAME=HOST_NAME)
-
-
-
                     msg['From'] = MY_ADDRESS
                     msg['To'] = CONSULTANT_EMAILS[0]
                     msg['Subject'] = "Customer Details, location: " + LOCATION
@@ -179,15 +198,49 @@ def main():
 
 
         elif menu_choice == 6:
-            pass
+            final_count = Dailytracker(today, todays_date,  str(casual_customer_count), str(build_customer_count), view_times)
+            final_count_string = final_count.current_day + "," + final_count.current_date + "," + final_count.casual_customer_count + "," + final_count.build_customer_count
+            for time in final_count.entry_times:
+                final_count_string += "," + str(time)
+
+            print("Saving final day count...")
+            out_file = open("entry_history.csv", 'a+')
+            out_file.write(final_count_string)
+            out_file.write("\n")
+            out_file.close()
+            print("Saved in entry_history.csv")
+            print("Goodbye")
+
+
+        elif menu_choice == 7:
+            confirm_report = input("This will send the last 7 days of information to your nominated consultant. Continue? (Y/N) ").upper()
+            if confirm_report == "Y":
+                template_7days_1 = REPORT_TEMPLATE_1.format(CONSULTANT=CONSULTANT_NAME[0], LOCATION=LOCATION)
+
+                template_7days_2 = ""
+                last_7_days = get_last_7_days("entry_history.csv")
+                for day in reversed(last_7_days):
+                    template_7days_2 += REPORT_TEMPLATE_2.format(DAY=day[0], DATE=day[1], CASUAL_CUSTOMERS=day[2], BUILD_CUSTOMERS=day[3], TIMES=day[4])
+                    print(template_7days_2)
+                template_7days_3 = REPORT_TEMPLATE_3.format(HOST_NAME=HOST_NAME)
+
+                total_template = template_7days_1 + template_7days_2 + template_7days_3
+                print(total_template)
+                #Construct message
+                #msg = MIMEMultipart()  # create a message
+                #msg['From'] = MY_ADDRESS
+                #msg['To'] = CONSULTANT_EMAILS[0]
+                #msg['Subject'] = "Weekly report, location: " + LOCATION
+                #msg.attach(MIMEText(template_7days_1 + template_7days_2 + template_7days_3))
+                #s.send_message(msg)
+                #print("Email sent.")
+                #del msg
 
         else:
             print("Invalid input")
-        print_menu(casual_customer_count, build_customer_count)
+        print_menu(casual_customer_count, build_customer_count, today)
         menu_choice = int(input(">>> "))
 
-    current_datetime = datetime.datetime.now()
-    today = current_datetime.strftime("%A")
 
     todays_count = Dailytracker(today, str(casual_customer_count), str(build_customer_count), view_times)
     today_string = todays_count.current_day + "," + todays_count.casual_customer_count + "," + todays_count.build_customer_count
@@ -201,11 +254,10 @@ def main():
     print("Goodbye")
 
 
-def print_menu(casual_count, build_count):
+def print_menu(casual_count, build_count, today):
     """Prints the main menu for customer counter"""
-    current_datetime = datetime.datetime.now()
-    print("Today is {}".format(current_datetime.strftime("%A")))
-    if current_datetime.strftime("%A") in REPORT_DAYS:
+    print("Today is {}".format(today))
+    if today in REPORT_DAYS:
         print("Today is a reporting day. Make sure to report after 3:30pm!")
     print("Current casual customer count: {}".format(casual_count))
     print("Current potential 6-12 months build customer count: {}".format(build_count))
@@ -215,8 +267,9 @@ def print_menu(casual_count, build_count):
     print("3. View entry times")
     print("4. Customer details")
     print("5. reload backup daily details (Use in case of accidental quit)")
-    print("6. Send weekly report")
-    print("7. save and exit")
+    print("6. End of day save ")
+    print("7. Send weekly report")
+    print("8. Exit")
 
 
 def print_customer_menu():
@@ -318,6 +371,18 @@ def update_details(choice, details, customer, customers):
         print("Enter a valid number.")
 
 
+def get_last_7_days(history):
+
+    last_7_lines = []
+    in_file = open(history, "r")
+    line_list = in_file.readlines()
+    in_file.close()
+    for line in line_list[-1:-8:-1]:
+        line = line.strip('\n')
+        line = line.split(',')
+        last_7_lines.append(line)
+    return last_7_lines
+
+
+
 main()
-
-
--- daily_tracker.py
+++ daily_tracker.py
@@ -1,9 +1,10 @@
 class Dailytracker():
     """Formulate and store the count for the day."""
 
-    def __init__(self, current_day = "", casual_customer_count = 0, build_customer_count = 0, entry_times = []):
+    def __init__(self, current_day = "", current_date = "", casual_customer_count = 0, build_customer_count = 0, entry_times = []):
 
         self.current_day = current_day
+        self.current_date = current_date
         self.casual_customer_count = casual_customer_count
         self.build_customer_count = build_customer_count
         self.entry_times = entry_times
